b75b5ebe5d7419e6d2b107136e3cd8df508c7b90
IgorMelnykSI <ihor.melnyk@etu.unice.fr>
2021-12-03 03:52:56 +0100

    Ajout de deux tables

--- jeuscript.js
+++ jeuscript.js
@@ -9,7 +9,7 @@ ctx.fill();
 ctx.closePath();
 */
 ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.arc(10, 10, 20, 0, Math.PI*2, false);
 ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
 ctx.stroke();
 ctx.closePath();
@@ -17,6 +17,8 @@ ctx.closePath();
 let x = 100;
 let y = 50;
 var ballRadius = 10;
+var isMouseDown = false;
+
 
 function drawCercle() {
     // le code pour dessiner
@@ -35,4 +37,4 @@ function draw() {
     y += 10;
   }
 
-  setInterval(draw, 50);
+  //setInterval(draw, 50);
